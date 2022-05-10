19e6b460c8698b07f4884a7deb15f454853839f8
Aymane El-yamoun <ael-yamo@e2r10p13.1337.ma>
2022-05-10 17:18:56 +0100

    fixing norm for death.c

--- philo/death.c
+++ philo/death.c
@@ -1,4 +1,16 @@
-#include "philosopher.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   death.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-yamo <ael-yamo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 14:53:42 by ael-yamo          #+#    #+#             */
+/*   Updated: 2022/05/10 16:59:35 by ael-yamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -21,31 +33,30 @@ void	free_data(t_free to_free)
 	free(to_free.philos_data->finish_mutex);
 	free(to_free.philos);
 	free(to_free.philos_data);
+	exit(0);
 }
 
 void	*check_death(t_philo *philos_data, t_free to_free)
 {
-	int	j;
+	int			j;
 	long int	time;
 
-	while (*(philos_data->start) != 1);
+	while (*(philos_data->start) != 1)
+		;
 	usleep(philos_data->data.time_to_die * 990);
 	while (1337)
 	{
 		if (*(philos_data->finish) == 0)
-		{
 			free_data(to_free);
-			exit(0);
-		}
 		j = 0;
 		time = get_time();
 		while (j < philos_data->data.philos_n)
 		{
 			if (time > philos_data[j].time_of_death)
 			{
-				printf("%ld\t%d\thas died\n", time - philos_data[j].time_of_birth, philos_data[j].ph_n);
+				printf("%ld\t%d\thas died\n", \
+				time - philos_data[j].time_of_birth, philos_data[j].ph_n);
 				free_data(to_free);
-				exit(0);
 			}
 			j++;
 		}
