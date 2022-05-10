fe6f95f699f53f4bb8901de134aa74ac79cbac9c
Aymane El-yamoun <ael-yamo@e2r10p13.1337.ma>
2022-05-10 17:20:50 +0100

    fixing norm for utils.c

--- philo/utils.c
+++ philo/utils.c
@@ -1,17 +1,29 @@
-#include "philosopher.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-yamo <ael-yamo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 15:12:56 by ael-yamo          #+#    #+#             */
+/*   Updated: 2022/05/10 16:56:38 by ael-yamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdlib.h>
 
-long int get_time(void)
+long int	get_time(void)
 {
 	struct timeval	time;
 
 	if (gettimeofday(&time, NULL) == -1)
 		exit_with_err("problem with gettimeofday\n", 2);
-	return (time.tv_sec * 1000) + (time.tv_usec / 1000);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
 }
 
 void	argv_data(int argc, char **argv, t_data *data)
@@ -30,18 +42,17 @@ void	argv_data(int argc, char **argv, t_data *data)
 			data->many_times_to_eat = atoi(argv[5]);
 		}
 	}
-
 	else
 		exit_with_err("somthing worong with the arguments", 3);
 }
 
-void    ft_usleep(long duration)
+void	ft_usleep(long duration)
 {
-    long            start;
+	long	start;
 
-    start = get_time();
-    while (get_time() - start < duration)
-        usleep(100);
+	start = get_time();
+	while (get_time() - start < duration)
+		usleep(100);
 }
 
 void	wait_all_philos(t_philo *philo)
